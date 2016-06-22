e453175c85b5e8b53f8ba7a81db77c20e17770f9
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-06-22 22:01:22 +0700

    show user activity for day

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -3,5 +3,6 @@ PublicActivity::Activity.class_eval do
     recipient_type: Settings.activity.course)}
   scope :user, -> (user_id) {where(owner_id: user_id, owner_type: "User")}
   scope :task, -> (task_id) {where(trackable_type: "Task", trackable_id: task_id)}
-  scope :course_subject -> (course_subject_id) {where(trackable_type: "CourseSubject", trackable_id: course_subject_id)}
+  scope :course_subject, -> (course_subject_id) {where(trackable_type: "CourseSubject", trackable_id: course_subject_id)}
+  scope :by_day, -> (day) {where("DAY(created_at) = ?", day)}
 end
