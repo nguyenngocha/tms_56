b8c1880ca60cdb88a6cc782f1204a74f47636b21
Trần Trung Hiếu <kagoromo@gmail.com>
2016-07-11 12:31:21 +0700

    sidekiq send notify mail 2 days before course finish

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,10 @@ class UserMailer < ApplicationMailer
     @course = course
     mail to: @user.email, subject: t("mail.delete", content: course.content)
   end
+
+  def will_finish_in_two_days course
+    @course = course
+    @user = @course.user
+    mail to: @user.email, subject: t("mail.will_finish", content: course.content)
+  end
 end
